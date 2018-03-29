7791a4faf8984257ae360e7a04a45d756a57b481
zhaofinger <zhaohaibo1994@gmail.com>
2018-03-30 00:10:40 +0800

    [fix] 配置文件修改

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -15,3 +15,41 @@ module.exports = appInfo => {
 
   return config;
 };
+
+// mysql 配置
+exports.mysql = {
+  // 单数据库信息配置
+  client: {
+    // host
+    host: '127.0.0.1',
+    // 端口号
+    port: '3306',
+    // 用户名
+    user: 'node',
+    // 密码
+    password: '',
+    // 数据库名
+    database: 'asi',
+  },
+  // 是否加载到 app 上，默认开启
+  app: true,
+  // 是否加载到 agent 上，默认关闭
+  agent: false,
+};
+
+// 跨域
+exports.security = {
+  domainWhiteList: ['localhost:3000'],
+  csrf: false,
+};
+exports.cors = {
+  allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+  credentials: true,
+};
+
+// 七牛配置
+exports.qiniu = {
+  accessKey: 'xxxxxx',
+  secretKey: 'xxxxxx',
+  scope: 'asi-dev'
+};
